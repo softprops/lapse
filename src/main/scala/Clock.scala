734ef68c1989f79e31c39af57bd76a26fcc8107d
softprops <d.tangren@gmail.com>
2014-08-26 08:27:17 -0400

    ditch the const clock as it wasn't really useful

--- src/main/scala/Clock.scala
+++ src/main/scala/Clock.scala
@@ -1,17 +1,15 @@
 package lapse
 
 /** Exposes a means of obtaining the current time */
-trait Clock {
+abstract class Clock {
   /** @return the current time in nanos */
   def read: Long
 }
 
 object Clock {
-  implicit val SystemClock: Clock = new Clock {
-    def read = System.nanoTime()  
-  }
-  /** @return a clock whose reading doesn't change */
-  def const(reading: Long): Clock = new Clock {
-    val read = reading
+  implicit val system: Clock = Clock(() => System.nanoTime)
+
+  def apply(now: () => Long): Clock = new Clock {
+    def read = now()
   }
 }
