91724ec0ec33921fead921bb8d5ba9dd0fb7bbfb
softprops <d.tangren@gmail.com>
2014-08-26 08:28:21 -0400

    introduce tupled, rename trait so java gets a proper static forwarder

--- src/main/scala/Stopwatch.scala
+++ src/main/scala/Stopwatch.scala
@@ -3,7 +3,7 @@ package lapse
 import scala.concurrent.duration.{ Duration, FiniteDuration }
 
 /** Tracks the passage of time */
-trait Stopwatch {
+trait Stopwatched {
   type Elapsed = () => FiniteDuration
 
   /** @return a function that, when applied, returns a duration from the start point */
@@ -15,17 +15,27 @@ trait Stopwatch {
    * @return result of applying f
    */
   def log[T]
-   (l: FiniteDuration => Unit)
-   (f: => T)
-   (implicit clock: Clock): T = {
-    val lap = start()
-    val res = f
-    l(lap())
-    res
-  } 
+    (l: FiniteDuration => Unit)
+    (f: => T)
+    (implicit clock: Clock): T = {
+      val (res, lap) = tupled(f)
+      l(lap)
+      res
+    }
+
+  /**
+   * @param f function to measure
+   * @return two element tuple of (result, duration)
+   */
+  def tupled[T]
+    (f: => T)
+    (implicit clock: Clock): (T, FiniteDuration) = {
+      val lap = start()
+      (f, lap())
+    }
 }
 
-object Stopwatch extends Stopwatch {
+object Stopwatch extends Stopwatched {
   def start()(implicit clock: Clock): Elapsed = {
     val since = clock.read
     () => Duration.fromNanos(clock.read - since)
